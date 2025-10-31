5177e871ce328e83d0cf3f6dd08f862833034093
kdw8219 <kdw8219@gmail.com>
2025-10-31 23:21:22 +0900

    Feature  - make docker compose to run on docker  - change host and port to connect docker internal

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 1234);
+  const host = '0.0.0.0';
+  await app.listen(process.env.PORT ?? 1234, host);
 }
 bootstrap();
